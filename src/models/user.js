c245a153f0fe24cf903cbdd6072d384583c23607
wojtek-krysiak <wojciech.g.krysiak@gmail.com>
2019-01-15 10:13:42 +0100

    implement sign up feature along with tests

---
+++ src/models/user.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+
+const UserSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    lowercase: true,
+    trim: true,
+  },
+  firstName: String,
+  lastName: String,
+})
+
+const User = mongoose.model('User', UserSchema)
+
+module.exports = User
